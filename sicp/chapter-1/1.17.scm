b9d16ab3c1515dcee0ce6d96f98a89d5abb8c20c
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-08-31 19:30:14 +0200

    task: 1.17

---
+++ sicp/chapter-1/1.17.scm
@@ -0,0 +1,17 @@
+(define (mult a b)
+  (if (= b 0)
+      0
+      (+ a (mult a (- b 1)))))
+
+(define (fast-mult a b)
+  (cond ((= b 0) 0)
+        ((even? b) (fast-mult (double a) (halve b)))
+        (else (+ a (fast-mult a (- b 1))))))
+
+(define (double x) (+ x x))
+(define (halve x) (/ x 2))
+
+;; Requires 512MB ram!!
+(mult 3 5000000)
+
+(fast-mult 3 5000000)
