7f99676ae619a69c974625db5682af7ab69fabbb
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-09-19 13:49:32 +0200

    task: 2.16

---
+++ sicp/chapter-2/2.16.scm
@@ -0,0 +1,5 @@
+;; I imagine it is a difficult problem, since it's valid to complicate an expression by adding more (opposite) operations.
+;; If you could simplify the expression to a simplest possible one algebraically, that would minimize the error bounds and could
+;; solve the issue of different representations producing different results.
+;; I guess this would require some symbolic manipulation or otherwise more complex representation of the intervals to be processed.
+;; You would have to know which intervals are equal.
