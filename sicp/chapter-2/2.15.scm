4b53e947af55e6037aef712c2d1a6ce37c1569fd
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-09-19 13:43:53 +0200

    task: 2.15

---
+++ sicp/chapter-2/2.15.scm
@@ -0,0 +1,4 @@
+;; Eva is correct.
+;; As touched on in the previous exercise, the primitiv operations on intervals involve their width.
+;; A less certain value is represented as a larger interval. Manipulating intervals make the uncertainty larger.
+;; Since one of the algebraic representations of paralell resistance calulations perform more of these to arrive at the same answer, it does so with less certainty.
