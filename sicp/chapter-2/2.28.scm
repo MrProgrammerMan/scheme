2816e5112062f7cda4eba704076d05e087ff0e6c
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-09-22 11:29:38 +0200

    task: 2.28

---
+++ sicp/chapter-2/2.28.scm
@@ -0,0 +1,8 @@
+(define (fringe t)
+  (cond ((null? t) '())
+        ((pair? t) (append (fringe (car t)) (fringe (cdr t))))
+        (else  (list t))))
+
+(define x (list (list 1 2) (list 3 4)))
+(fringe x)
+(fringe (list x x))
